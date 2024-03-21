5439b9551fc4d3d59ffbee5faee912ba7f7cec6a
irtazafayaz <irtazab135@gmail.com>
2024-03-21 17:56:26 +0500

    Project Starter

---
+++ GroupChat/GroupChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupChatApp.swift
+//  GroupChat
+//
+//  Created by Irtaza Fiaz on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GroupChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
